d2ca71dc9fdf121614adb5d667cee7da9e01019a
100Gyeon <bjy9966@naver.com>
2021-09-11 21:19:04 +0900

    Firebase setup

---
+++ twitter/src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxAoacJ-jP94V-BEMKsIzciT3dE1wXkKw",
+  authDomain: "twitter-7819c.firebaseapp.com",
+  projectId: "twitter-7819c",
+  storageBucket: "twitter-7819c.appspot.com",
+  messagingSenderId: "234695651509",
+  appId: "1:234695651509:web:2c766cf88676f4e7624efc"
+};
+
+export default firebase.initializeApp(firebaseConfig);
