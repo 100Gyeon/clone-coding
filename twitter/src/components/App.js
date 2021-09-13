7276dd79147c7385149a31dbf6f23d6ce6ff11c6
100Gyeon <bjy9966@naver.com>
2021-09-13 21:12:24 +0900

    Use firebase auth

--- twitter/src/components/App.js
+++ twitter/src/components/App.js
@@ -1,8 +1,15 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase"; // jsconfig 덕분에 이런 식으로 import 가능
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); // 로그인 여부 판별
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Twitter</footer>
+    </>
+  );
 }
 
 export default App;
