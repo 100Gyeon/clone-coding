80e63e4647e31bf36bc1da9f857c903859ff8e89
100Gyeon <bjy9966@naver.com>
2021-09-16 13:36:16 +0900

    Logout

--- twitter/src/components/Router.js
+++ twitter/src/components/Router.js
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile"
+import Navigation from "components/Navigation"
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            {/* 아래는 그 외의 route로 가게 되면 "/"로 돌아가라는 의미 */}
+            {/* <Redirect from="*" to ="/" /> */}
           </>
         ) : (
-          <Route exact path="/">
-            {" "}
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              {" "}
+              <Auth />
+            </Route>
+            <Redirect from="*" to ="/" />
+          </>
         )}
       </Switch>
     </Router>
--- twitter/src/routes/Profile.js
+++ twitter/src/routes/Profile.js
@@ -1,3 +1,16 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router";
 
-export default () => <span>Profile</span>;
+export default () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/"); // 로그아웃된 후 다시 home으로 돌아갈 수 있도록
+  }
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
