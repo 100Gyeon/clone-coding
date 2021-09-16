639dcad1b71950da96ec3f34edce2bd324e5d74b
100Gyeon <bjy9966@naver.com>
2021-09-16 12:16:35 +0900

    Sign in

--- twitter/src/routes/Auth.js
+++ twitter/src/routes/Auth.js
@@ -30,7 +30,8 @@ const Auth = () => {
     } catch (error) {
       setError(error.message);
     }
-  }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -50,9 +51,10 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
         {error}
       </form>
+      <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
       <div>
         <button>Continue with Google</button>
       </div>
