17373484947d49a8b1417ca2da1167e98a2488ed
100Gyeon <bjy9966@naver.com>
2021-09-24 23:58:40 +0900

    Form setup

--- twitter/src/routes/Home.js
+++ twitter/src/routes/Home.js
@@ -1,4 +1,11 @@
 import React from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  return (<div>
+    <form>
+      <input type="text" placeholder="What's on your mind?" maxLength={120} />
+      <input type="submit" value="Tweet" />
+    </form>
+  </div>);
+}
 export default Home;
