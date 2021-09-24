e384375d0589bb6bca967d6be5e1149301616b54
100Gyeon <bjy9966@naver.com>
2021-09-25 00:07:41 +0900

    Form setup

--- twitter/src/routes/Home.js
+++ twitter/src/routes/Home.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+  const onChange = (event) => {
+    // event로부터 target 안에 있는 value를 달라고 하는 것
+    const { target: { value } } = event;
+    setTweet(value);
+  }
   return (<div>
-    <form>
-      <input type="text" placeholder="What's on your mind?" maxLength={120} />
+    <form onSubmit={onSubmit}>
+      <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
       <input type="submit" value="Tweet" />
     </form>
   </div>);
