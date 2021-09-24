9906dc28049d06fc25119f7502578f2724125c7d
100Gyeon <bjy9966@naver.com>
2021-09-25 00:45:00 +0900

    Firestore setup

--- twitter/src/routes/Home.js
+++ twitter/src/routes/Home.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import { dbService } from "fbase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
-  }
+    console.log(`submit 하는 트윗: ${tweet}`);
+    await addDoc(collection(dbService, "tweets"), {
+      tweet,
+      createdAt: serverTimestamp(),
+    });
+    setTweet("");
+  };
   const onChange = (event) => {
     // event로부터 target 안에 있는 value를 달라고 하는 것
     const { target: { value } } = event;
