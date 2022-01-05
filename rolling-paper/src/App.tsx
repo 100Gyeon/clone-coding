5ee1eef3ba33ff25051c8584ba4c8626ff273bc6
100Gyeon <bjy9966@naver.com>
2022-01-06 03:00:20 +0900

    feat: Recoil 적용

--- rolling-paper/src/App.tsx
+++ rolling-paper/src/App.tsx
@@ -1,12 +1,15 @@
 import { Global } from '@emotion/react';
 import { GlobalStyles } from './components/common/GlobalStyles';
+import { RecoilRoot } from 'recoil';
 import Main from './pages/Main';
 
 function App() {
   return (
     <>
       <Global styles={GlobalStyles} />
-      <Main />
+      <RecoilRoot>
+        <Main />
+      </RecoilRoot>
     </>
   );
 }
